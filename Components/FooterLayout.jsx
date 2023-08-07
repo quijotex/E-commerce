f2e594caf9cd4fbbb6c01a8d53b920ba413fc4d1
Mario Martinez <mariopresiga@gmail.com>
2023-08-07 17:49:26 -0500

    Footer is already done

--- Components/FooterLayout.jsx
+++ Components/FooterLayout.jsx
@@ -5,7 +5,19 @@ const FooterLayout = () => {
         <div className="layout">
                 <Outlet/>
             <footer className="footer">
-                <h1>Academlo</h1>
+                <h1>Created by</h1>
+                <div className="footer__developers">
+                    <div className="developer-Mario">
+                        <h2>Mario Martínez</h2>
+                        <a name="LinkedIn" href="https://www.linkedin.com/in/mario-mart%C3%ADnez-961393189/" target="_blank"><i className='bx bxl-linkedin-square'></i></a>
+                        <a name="GitHub" href="https://github.com/quijotex" target="_blank"><i className='bx bxl-github' ></i></a>
+                    </div>
+                    <div className="developer-Helson">
+                        <h2>Helson Manchego</h2>
+                        <a name="LinkedIn" href="https://www.linkedin.com/in/helson-manchego-b5782836/" target="_blank"><i className='bx bxl-linkedin-square'></i></a>
+                        <a name="GitHub"  href="https://github.com/helsontm" target="_blank"><i className='bx bxl-github' ></i></a>
+                    </div>
+                </div>
             </footer>
         </div>
     )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -33,9 +33,8 @@ const Home = () => {
 
     return(
         <main>
-                <Row>
-
-                    <Col md={4} lg={3}>
+            <Row>
+                <Col md={4} lg={3}>
                     <ListGroup>
                         {
                             categories?.map( category => (
@@ -46,57 +45,56 @@ const Home = () => {
                             ))
                         }
                     </ListGroup>
-                    </Col>
-                    <Col md={8} lg={9}>
+                </Col>
+                <Col md={8} lg={9} className='col-product-list'>
                     <Row>
                         <Col>
-                        <InputGroup className="searcher">
-                            <Form.Control
-                            placeholder="What are you looking for?"
-                            id='IdSearch'
-                            value={searchValue}
-                            onChange={e => setSearchValue(e.target.value)}
-                            />
-                           <Button  onClick={() => dispatch(filterProductsByNameThunk(searchValue) )} className='searcher-button'><i className='bx bx-search'></i></Button>
-                        </InputGroup>
+                            <InputGroup className="searcher">
+                                <Form.Control
+                                placeholder="What are you looking for?"
+                                id='IdSearch'
+                                value={searchValue}
+                                onChange={e => setSearchValue(e.target.value)}
+                                />
+                            <Button  onClick={() => dispatch(filterProductsByNameThunk(searchValue) )} className='searcher-button'><i className='bx bx-search'></i></Button>
+                            </InputGroup>
                         </Col>
                     </Row>
-                        <Row  xs={1} md={2} lg={3}>
-                            {
-                                products?.map(product => (
+                    <Row  xs={1} md={2} lg={3}>
+                            {products?.map(product => (
                                 <Col className="li-products"  key={product.id}>
-                                     <Card className="card-product__detail" >
-                                <a className="card-product__anchor" href={`#/products/${product.id}`} > 
-                                    <div className="card-product__images">
-                                    <Card.Img className="card-detail__img1" variant="top" src={product?.images[0]?.url} />
-                                    <Card.Img className="card-detail__img2" variant="top" src={product?.images[1]?.url} />
-                                    </div>
-                                     <Card.Body className="card-product__body">
-                                         <Card.Title> <span className="card-body__gray card-body__gray--brand">{product?.brand}</span> </Card.Title>
-                                         <Card.Text>
-                                           <span className="card-body__bold">{product?.title}</span> 
-                                         </Card.Text>
-                                         <div className="card-product__price">
+                                    <Card className="card-product__detail" >
+                                        <a className="card-product__anchor" href={`#/products/${product.id}`} > 
+                                        <div className="card-product__images">
+                                        <Card.Img className="card-detail__img1" variant="top" src={product?.images[0]?.url} />
+                                        <Card.Img className="card-detail__img2" variant="top" src={product?.images[1]?.url} />
+                                        </div>
+                                        <Card.Body className="card-product__body">
+                                            <Card.Title> <span className="card-body__gray card-body__gray--brand">{product?.brand}</span> </Card.Title>
                                             <Card.Text>
-                                                 <span className="card-body__gray">Price</span>
+                                            <span className="card-body__bold">{product?.title}</span> 
                                             </Card.Text>
-                                            <Card.Text>
-                                                <span className="card-body__bold">{product?.price}</span> 
-                                            </Card.Text>
-                                         </div>
-                                     </Card.Body>
-                                 </a>
-                                 <div className="card-product__button" >
-                                    <Button variant="primary"><i className='bx bx-cart'></i></Button>
-                                 </div>
-                             </Card>
-                                    </Col>
+                                            <div className="card-product__price">
+                                                <Card.Text>
+                                                    <span className="card-body__gray">Price</span>
+                                                </Card.Text>
+                                                <Card.Text>
+                                                    <span className="card-body__bold">{product?.price}</span> 
+                                                </Card.Text>
+                                            </div>
+                                        </Card.Body>
+                                        </a>
+                                        <div className="card-product__button" >
+                                        <Button variant="primary"><i className='bx bx-cart'></i></Button>
+                                        </div>
+                                     </Card>
+                                </Col>
                                     )
                                 )
                             }
-                        </Row>
-                   </Col>
-                </Row>
+                    </Row>
+                 </Col>
+            </Row>
             
         </main>
     )
