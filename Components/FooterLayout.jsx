6fcc54af6bf755b26455b971b3429116322e90ee
Mario Martínez <mariopresiga@gmail.com>
2023-12-20 14:15:14 -0500

    adding alt

--- Components/FooterLayout.jsx
+++ Components/FooterLayout.jsx
@@ -11,13 +11,13 @@ const FooterLayout = () => {
                 <div className="footer__developers">
                     <div className="developer-Mario">
                         <h2>Mario Martínez</h2>
-                        <a name="LinkedIn" href="https://www.linkedin.com/in/mario-mart%C3%ADnez-961393189/" target="_blank"><img src={LinkedIn} alt="LinkedIn"/></a>
-                        <a name="GitHub" href="https://github.com/quijotex" target="_blank"><img src={GitHub} alt="GitHub"/></a>
+                        <a name="LinkedIn" href="https://www.linkedin.com/in/mario-mart%C3%ADnez-961393189/" target="_blank" rel="noreferrer"><img src={LinkedIn} alt="LinkedIn"/></a>
+                        <a name="GitHub" href="https://github.com/quijotex" target="_blank" rel="noreferrer"><img src={GitHub} alt="GitHub"/></a>
                     </div>
                     <div className="developer-Helson">
                         <h2>Helson Manchego</h2>
-                        <a name="LinkedIn" href="https://www.linkedin.com/in/helson-manchego-b5782836/" target="_blank"><img src={LinkedIn} alt="LinkedIn"/></a>
-                        <a name="GitHub"  href="https://github.com/helsontm" target="_blank"><img src={GitHub} alt="GitHub"/></a>
+                        <a name="LinkedIn" href="https://www.linkedin.com/in/helson-manchego-b5782836/" target="_blank" rel="noreferrer"><img src={LinkedIn} alt="LinkedIn"/></a>
+                        <a name="GitHub"  href="https://github.com/helsontm" target="_blank" rel="noreferrer"><img src={GitHub} alt="GitHub"/></a>
                     </div>
                 </div>
             </footer>
