92b6abe6b2ae7f3e2b712efbba5ebd1b7130aa25
Mario Martínez <mariopresiga@gmail.com>
2023-12-20 13:26:01 -0500

    enhancing responsive design

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import "bootswatch/dist/lux/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import store from './store';
 import { Provider } from 'react-redux';
